245ad8bb2d06308d101526bc7d01c7637890ef26
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-06-15 13:42:56 +0200

    feat: testing

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,21 @@
 import vituum from 'vituum'
 import liquid from '@vituum/vite-plugin-liquid'
+import twig from '@vituum/vite-plugin-twig'
 import juice from '@vituum/vite-plugin-juice'
 
 export default {
     plugins: [
-        vituum(),
+        vituum({
+            input: ['./src/main.js', './src/main.css']
+        }),
         liquid(),
+        twig(),
         juice({
             paths: ['src/email']
         })
     ],
     build: {
         manifest: true,
-        modulePreload: false,
-        rollupOptions: {
-            input: [
-                './src/emails/**/*.{liquid,html,json}',
-                './src/pages/**/*.{liquid,html,json}',
-                '!./src/pages/**/*.{liquid,html}.json',
-                './src/main.js',
-                './src/main.css'
-            ]
-        }
+        modulePreload: false
     }
 }
